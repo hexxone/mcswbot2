c9a794ca1eddcdeed767b6bf5acc2befb795bf48
hexx.one <5312542+hexxone@users.noreply.github.com>
2020-01-20 20:50:51 +0100

    Switching to dynamic JSON access..

--- mcswbot2/Program.cs
+++ mcswbot2/Program.cs
@@ -7,9 +7,16 @@ namespace mcswbot2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("MineCraftServerWatchBotV2 init...");
-
             TgBot.Start();
+        }
+
+        /// <summary>
+        ///     DateTime Wrapper for Console WriteLine
+        /// </summary>
+        /// <param name="l"></param>
+        public static void WriteLine(string l)
+        {
+            Console.WriteLine("[" + DateTime.Now.ToString("yyyy-MM-ss HH:mm:ss") + "] " + l);
         }
     }
 }
